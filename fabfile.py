bd41c5e73c5763725b600dcf62d6ca9864b7a341
Jeff Soule <jeff@soule.me>
2016-05-06 09:11:18 -0700

    Working on reboot logic

--- fabfile.py
+++ fabfile.py
@@ -6,13 +6,17 @@ from fabric import tasks
 from fabric.network import disconnect_all
 from fabric.contrib.console import confirm
 import re
-# import os
+import os
 
 
 def update():
-            sudo("yum -y update --disablerepo='*artifactory' --exclude=puppet* --exclude=sensu --exclude=mongo* --exclude=redis* --exclude=rabbitmq*", pty=True)
+	"""Run yum update with exclusions"""
+	sudo("yum -y update --disablerepo='*artifactory' --exclude=puppet* --exclude=sensu --exclude=mongo* --exclude=redis* --exclude=rabbitmq*", pty=True)
 
-
-
+def slowReboot():
+	"""Do a careful reboot with checks."""
+	run("uname -r")
+	reboot(wait=120)
+	run("uname -r")
 
 

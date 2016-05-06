7fd11e8111787303e68524fb314819dac5491e88
Jeff Soule <jeff@soule.me>
2016-05-06 09:30:50 -0700

    Testing

--- fabfile.py
+++ fabfile.py
@@ -18,7 +18,7 @@ def slowReboot():
 	print("Get the kernel version before reboot.")
 	preresult = run("uname -r")
 	preresult.failed
-	print($result)
+	# print($result)
 	# reboot(wait=120)
 	print("Get the kernel version after reboot.")
 	run("uname -r")

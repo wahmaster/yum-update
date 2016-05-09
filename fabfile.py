d6a21bf595cf5adcef3519f981ab986b54d48f26
Jeff Soule <jeff@soule.me>
2016-05-09 08:50:58 -0700

    Testing

--- fabfile.py
+++ fabfile.py
@@ -35,4 +35,4 @@ def updatetest():
                 """Run yum update with exclusions"""
                 env.parallel = True
 		print ("These are the excludes: %s") % (env.excludes)				
-                # sudo("yum update --disablerepo='*artifactory' %s", pty=True) % excludes
+                sudo("yum update --disablerepo='*artifactory' %s", pty=True) % (env.excludes)

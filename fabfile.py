5430f6f12932b33a69407d7ee4cb0e3ad225ea6e
Jeff Soule <jeff@soule.me>
2016-05-25 08:28:30 -0700

    Testing

--- fabfile.py
+++ fabfile.py
@@ -55,13 +55,14 @@ def slowReboot():
 @excludehosts
 def fastReboot():
 	"""Do a fast reboot with checks."""
-	print "<font color=red>Rebooting %s now!</font>" % env.host
-	preresult = run("uname -r")
-	preresult.failed
-	print "<font color=red>%s kernel version before reboot:</font><font color=green> %s</font>" % (env.host, preresult)
-	""" reboot(wait=120) """
-	postresult = run("uname -r")
-	print "<font color=red>%s kernel version after reboot: <font color=green> %s</font>" % (env.host, preresult)
+	with hide('everything'):
+	    print "<font color=red>Rebooting %s now!</font>" % env.host
+	    preresult = run("uname -r")
+	    preresult.failed
+	    print "<font color=red>%s kernel version before reboot:</font><font color=green> %s</font>" % (env.host, preresult)
+	    """ reboot(wait=120) """
+	    postresult = run("uname -r")
+	    print "<font color=red>%s kernel version after reboot: <font color=green> %s</font>" % (env.host, preresult)
 
 @task
 @parallel

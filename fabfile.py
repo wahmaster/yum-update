e2633bd05fc4076e1ad02e61f5cf21d93b5358ec
Jeff Soule <jeff@soule.me>
2016-05-25 08:21:59 -0700

    Testing

--- fabfile.py
+++ fabfile.py
@@ -61,7 +61,7 @@ def fastReboot():
 	print "<font color=red>%s kernel version before reboot:</font><font color=green> %s</font>" % (env.host, preresult)
 	""" reboot(wait=120) """
 	postresult = run("uname -r")
-	print "<font color=red>$s kernel version after reboot: <font color=green> %s</font>" % (env.host, preresult)
+	print "<font color=red>%s kernel version after reboot: <font color=green> %s</font>" % (env.host, preresult)
 
 @task
 @parallel

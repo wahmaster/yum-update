3222c269367c003b7d893861d32b777ace38a1c8
Jeff Soule <jeff@soule.me>
2016-06-08 07:39:47 -0700

    Testing

--- fabfile.py
+++ fabfile.py
@@ -92,7 +92,9 @@ def kernelReport():
 	with hide('everything'):
 		env.parallel = True
 		result = run("uname -r")
+        redhat = run("cat /etc/redhat-release")
 		print "<font color=green>%s is running kernel version: </font><font color=red>%s</font>" % (env.host, result)
+		print "<font color=green>%s contents of /etc/redhat-release: </font><font color=red>%s</font>" % (env.host, redhat)
 
 @task
 @parallel

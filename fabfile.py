b8c5696d7be060cd2c415e0d699c52492592bc79
Jeff Soule <jeff@soule.me>
2017-06-20 08:09:37 -0700

    increased timeout yet some more...

--- fabfile.py
+++ fabfile.py
@@ -52,7 +52,7 @@ def DoTheReboot():
 	    preresult = run("uname -r")
 	    preresult.failed
 	    print "<font color=red>%s kernel version before reboot:</font><font color=green> %s</font>" % (env.host, preresult)
-	    reboot(wait=45)
+	    reboot(wait=120)
         postresult = run("uname -r")
         print "<font color=red>%s Kernel version after reboot: <font color=green> %s</font>" % (env.host, postresult)
 
